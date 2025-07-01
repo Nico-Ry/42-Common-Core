5f2c2605b2b8158deaa583749ce342f6a253fdc6
nico ryser <nryser@student.42lausanne.ch>
2025-07-01 15:09:06 +0200

    adding cpp00

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,28 @@
+#include "PhoneBook.hpp"
+
+int main()
+{
+	PhoneBook phoneBook;
+	std::string command;
+
+	std::cout << YELLOW << "--Available commands: ADD, SEARCH, EXIT--" << RESET << std::endl;
+	while (1) {
+		std::cout << "Enter command: ";
+		if (!(std::cin >> command)) {
+			std::cout << std::endl << RED << "Exiting..." << RESET <<std::endl;
+			break;
+		}
+
+		if (command == "ADD")
+			phoneBook.addContact();
+		else if (command == "SEARCH")
+			phoneBook.searchContact();
+		else if (command == "EXIT") {
+			std::cout << RED << "Exiting..." << RESET << std::endl;
+			return (0);
+		} else {
+			std::cout << RED << "Invalid command" << RESET << std::endl;
+		}
+	}
+	return (0);
+}
