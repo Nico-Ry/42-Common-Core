29660e58593d073afe2246efc6b617cbac147bc1
nico ryser <nryser@student.42lausanne.ch>
2025-07-01 15:19:35 +0200

    added all cpp00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -5,8 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nryser <nryser@student.42lausanne.ch>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/24 16:12:21 by nryser            #+#    #+#             */
-/*   Updated: 2025/05/24 16:12:31 by nryser           ###   ########.ch       */
+/*   Created: 2025/07/01 13:59:42 by nryser            #+#    #+#             */
+/*   Updated: 2025/07/01 13:59:42 by nryser           ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+			if (i != argc - 1 && argv[i][0] != '\0')
+				std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
